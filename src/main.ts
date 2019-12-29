d326b4002cd2fb37d7684570e1eb3f9602c7d0bd
Alexis Hushbeck <github@imgineme.com>
2019-12-29 11:43:07 -0800

    Fix main window reactivation check

--- src/main.ts
+++ src/main.ts
@@ -43,7 +43,7 @@ app.on("window-all-closed", () => {
 app.on("activate", () => {
 	// On OS X it"s common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
+	if (!windower.hasMainWindow()) {
 		logger.info("Application is being reactivated from being closed");
 		bringAppUp();
 	}
