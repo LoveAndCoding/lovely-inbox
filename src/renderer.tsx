2cfa3c80bb61f07411e8780a33693e21bf9b1966
Alexis Hushbeck <github@imgineme.com>
2020-03-01 15:12:10 -0800

    :lipstick: Stop Loading CSS Via Unused Import
    Prior to this change, we were loading our shared CSS file in a gross
    way. We would import it in one of our TS files and rely on injection to
    be done when we accessed the file. Which is bonkers.
    
    Now, we do so much cleaner. We just include the base.css file in the
    html file. Like it should be. No more weird "we have to import this here
    so we get CSS" non-sense.

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -2,8 +2,6 @@ import * as ReactDOM from "react-dom";
 
 import app from "./ui/app";
 
-import "../styles/base.css";
-
 const mainEl = document.getElementById("root");
 
 if (!mainEl) {
--- webpack.rules.js
+++ webpack.rules.js
@@ -30,6 +30,9 @@ module.exports = [
 		test: /\.css$/,
 		use: [
 			{
+				loader: "style-loader",
+			},
+			{
 				loader: "css-loader",
 			},
 		],
