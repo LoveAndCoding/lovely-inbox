10e382472b1732cbfd887e3d6561200b5df3a11a
Alexis Hushbeck <github@imgineme.com>
2020-05-16 20:47:25 -0700

    :point_right: Move WindowManager Initialization
    Moving the WindowManager initialization to that folder. This is so that
    we can access the WindowManager without having to pass it around.

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,10 @@ import { app, BrowserWindow, Tray } from "electron";
 import { ApplicationConfig, UserConfig } from "./config";
 import logger from "./logger";
 import setupUnhandledListeners from "./process.error.handlers";
-import { createAppRouter } from "./route";
-import WindowManager from "./window/window.manager";
+import WindowManager from "./window";
 
 // Before we do anything else, setup handlers for any missed errors
 setupUnhandledListeners();
-const appRouter = createAppRouter();
-
-// Create a new WindowManager which we'll use to control all of our windows
-const windower = new WindowManager(appRouter);
 
 /**
  * bringAppUp
@@ -26,10 +21,10 @@ function bringAppUp() {
 
 	if (UserConfig.completedOnBoarding) {
 		logger.info("User has completed onboarding; Load Inbox");
-		windower.openInbox();
+		WindowManager.openInbox();
 	} else {
 		logger.info("User has not completed onboarding; Run them through it");
-		windower.openOnboarding();
+		WindowManager.openOnboarding();
 	}
 }
 
@@ -47,7 +42,7 @@ app.on("window-all-closed", () => {
 app.on("activate", () => {
 	// On OS X it"s common to re-create a window in the app when the
 	// dock icon is clicked and there are no other windows open.
-	if (!windower.hasMainWindow()) {
+	if (!WindowManager.hasMainWindow()) {
 		logger.info("Application is being reactivated from being closed");
 		bringAppUp();
 	}
---
+++ src/window/index.ts
@@ -0,0 +1,4 @@
+import { createAppRouter } from "../route";
+import WindowManager from "./window.manager";
+
+export default new WindowManager(createAppRouter());
